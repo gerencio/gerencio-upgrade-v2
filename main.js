5923561c47716cfea3aca1c82065d8a0f04420aa
Robert <robzhu@gmail.com>
2015-09-16 16:18:41 -0400

    initial checkin

---
+++ main.js
@@ -0,0 +1,87 @@
+util        = require('util');
+yaml        = require('js-yaml');
+writeYaml   = require('write-yaml');
+fs          = require('fs');
+
+var sourceComposeFile   = process.argv[2];  // the full path of the docker-compose.yml file that describes the current rancher stack
+var serviceName         = process.argv[3];  // the name of the service to upgrade
+var newServiceImage     = process.argv[4];  // the image of the new service, ex: ezephoenix/webspike:34
+
+filter_keys = function(obj, filter) {
+  var key, keys = [];
+  for (key in obj) {
+    if (obj.hasOwnProperty(key) && key.match(filter)) {
+      keys.push(key);
+    }
+  }
+  return keys;
+}
+
+try {
+  console.log("loading %s", sourceComposeFile);
+  var yamlDoc = yaml.safeLoad(fs.readFileSync(sourceComposeFile, "utf8"));
+  console.log("searching for service definition: %s", serviceName);
+
+  var expression = util.format("^%s*",serviceName);
+  var matches = filter_keys(yamlDoc, expression);
+
+  var currentServiceEntry = null;
+  if( matches.length == 0 ){
+    throw util.format("could not find any services matching name: %s", serviceName);
+  }
+  else if( matches.length == 1){
+    currentServiceEntry = matches[0];
+  }
+  else{
+    console.log("multiple service entries found that match: '%s': %s ", serviceName, matches );
+
+    var maxVersion = 0;
+    matches.forEach(function(entry){
+        var entryVersion = entry.split('-').pop();
+        if(entryVersion > maxVersion){
+            maxVersion = entryVersion;
+            currentServiceEntry = entry;
+        }
+    });
+  }
+  if(currentServiceEntry == null) {
+    throw "could not find a matching service entry, giving up";
+  }
+
+  console.log("Using service entry: " + currentServiceEntry);
+
+  //TODO: check the docker registry to see if the image actually exists
+  var currentServiceElement = yamlDoc[currentServiceEntry];
+  console.log(currentServiceElement);
+  //clone the service element
+  var newServiceElement = (JSON.parse(JSON.stringify(currentServiceElement)));
+  newServiceElement.image = newServiceImage;
+
+  //name the new service: 
+  var newServiceName = util.format( "%s-%s", serviceName, newServiceImage.split(':').pop() );
+
+  console.log("inserting new YAML element with name: %s", newServiceName );
+  yamlDoc[newServiceName] = newServiceElement;
+  
+  var targetFile = "./docker-compose-new.yml";
+  console.log("writing modified YAML file out to %s", targetFile);
+  writeYaml.sync(targetFile, yamlDoc);
+  console.log("successfully wrote modified YAML file out to %s", targetFile);
+  
+  var args = util.format("--url %s --access-key %s --secret-key %s -p %s --file %s upgrade %s %s", 
+    process.env.RANCHER_URL, 
+    process.env.RANCHER_ACCESS_KEY, 
+    process.env.RANCHER_SECRET_KEY, 
+    process.env.RANCHER_STACK,
+    targetFile, 
+    currentServiceEntry,
+    newServiceName );
+
+  console.log("now, just run:");
+  var cmd = "rancher-compose " + args;
+  console.log(cmd);
+  return cmd;
+} catch (e) {
+    console.log("Deployment failed:")
+    console.error(e);
+}
